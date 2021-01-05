ce997de5ecb49913a25e57017accff7995039407
Tejas Joshi <tejasjoshi689@gmail.com>
2021-01-05 07:55:31 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -2,5 +2,5 @@ module.exports = {
     database:"mongodb://localhost:27017/emaildb",
     secret:'yoursecret',
     host:'smpt.gmail.com',
-    pass:'69917070gaandiv7'
+    pass:'69917070gaandiv'
 }
